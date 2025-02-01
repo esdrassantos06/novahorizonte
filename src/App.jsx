0dd4cb41b098d7a77988ef1f720c840ea9fac165
esdrassantos06 <esdrasirion1@gmail.com>
2025-02-01 13:57:24 +0000

    Cookie Banner feito

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
+import CookiesBanner from './components/CookiesBanner'
 function App() {
 
 
   return (
     <div className='App min-h-screen w-full'>
+    <CookiesBanner/>
     <Outlet/>
     </div>
   )
--- src/components/CookiesBanner.jsx
+++ src/components/CookiesBanner.jsx
@@ -1,8 +1,38 @@
+import { useState, useEffect } from "react";
+import Cookies from "js-cookie";
+import { Link } from "react-router-dom";
+
+// Irei utilizar a Lib js-cookie
 
 const CookiesBanner = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    if (!Cookies.get("cookiesAccepted")) {
+      setIsVisible(true);
+    }
+  }, []);
+
+  const handleAccept = () => {
+    Cookies.set("cookiesAccepted", "true", { expires: 30 }); // Expira em 30 dias
+    setIsVisible(false);
+  };
+
   return (
-    <div>CookiesBanner</div>
-  )
-}
+    isVisible && (
+      <div className="fixed z-50 bottom-0 left-0 bg-neutral-800 right-0 text-white p-4 flex justify-between items-center">
+      <p>
+        Usamos cookies para melhorar sua experiência. Ao continuar, você concorda com nossa 
+        <Link to='/cookies' target="_blank" className="text-secondary hover:underline"> Política de Cookies</Link>.
+      </p>
+      <button 
+        onClick={handleAccept} 
+        className="bg-secondary cursor-pointer text-white px-4 py-2 rounded hover:bg-amber-600">
+        Aceitar
+      </button>
+    </div>
+    )
+  );
+};
 
 export default CookiesBanner

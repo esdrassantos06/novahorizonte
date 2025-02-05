9f0b9856262c0ac4fd5a33aff14d63d77ffa1a11
esdrassantos06 <esdrasirion1@gmail.com>
2025-02-05 22:28:07 +0000

    começando "quem somos"

--- src/routes/QuemSomos.jsx
+++ src/routes/QuemSomos.jsx
@@ -6,23 +6,23 @@ function QuemSomos() {
     <>
       <Navbar />
       <main className="w-full bg-primary pt-20 min-h-screen">
-        <section className="about-us-container w-full flex h-[80vh]">
+        <section className="about-us-container w-full flex h-screen">
 
-          <div className="w-full flex items-center justify-center bg-secondary">
+          <div className="w-full flex items-center justify-center bg-primary">
             <div className="title-about-us">
               <h1 className="text-text ">Sobre Nós</h1>
             </div>
           </div>
         </section>
 
-        <section className="our-mission-container w-full flex justify-center items-center h-screen">
+        <section className="our-mission-container w-full flex bg-secondary justify-center items-center h-screen">
           <div className="title-mission">
             <h1 className="text-text ">Nossa Missão</h1>
           </div>
         </section>
 
 
-        <section className="our-team-container w-full flex justify-center bg-secondary items-center h-[65vh]">
+        <section className="our-team-container w-full flex justify-center bg-tertiary items-center h-[65vh]">
           <div className="title-our-team">
             <h1 className="text-text ">Nosso Time</h1>
           </div>

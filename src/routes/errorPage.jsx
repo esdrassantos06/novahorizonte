1b72674131aa402aa8fa0b606c66015700dfb8d2
esdrassantos06 <esdrasirion1@gmail.com>
2025-02-17 15:23:29 +0000

    error page responsive

--- src/routes/errorPage.jsx
+++ src/routes/errorPage.jsx
@@ -5,10 +5,10 @@ const ErrorPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
       <h1 className="circularBlack text-9xl text-gray-800">404</h1>
-      <h2 className="circularMedium mb-4 text-4xl text-gray-600">
+      <h2 className="circularMedium text-center mb-4 text-4xl text-gray-600">
         Página Não Encontrada
       </h2>
-      <p className="circularBook mb-8 text-gray-500">
+      <p className="circularBook text-center mb-8 text-gray-500">
         A página que procura não existe ou foi movida.
       </p>
 

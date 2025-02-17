b93f9f2d278a5224bcfc3f3f934adba3bda49146
esdrassantos06 <esdrasirion1@gmail.com>
2025-02-17 14:05:04 +0000

    scrolls to the top when changing pages

---
+++ src/components/ScrollToTop.jsx
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+// Quando troca de páginas, ele rola pro topo da página automaticamente
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
+export default ScrollToTop;

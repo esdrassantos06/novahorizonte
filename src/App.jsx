69adcc090af867cf0731142218ae5ec228b2c177
esdrassantos06 <esdrasirion1@gmail.com>
2025-02-17 14:05:13 +0000

    scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import CookiesBanner from "./components/CookiesBanner";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <div className="App min-h-screen w-full">
+      <ScrollToTop />
       <CookiesBanner />
       <Outlet />
     </div>

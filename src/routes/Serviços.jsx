80de9e4b5818354618abfd2bc158ae1f7a0c1561
esdrassantos06 <esdrasirion1@gmail.com>
2025-02-17 13:32:52 +0000

    services responsive

--- src/routes/Serviços.jsx
+++ src/routes/Serviços.jsx
@@ -51,26 +51,28 @@ function Serviços() {
   return (
     <>
       <Navbar />
-      <main className="Serviços bg-primary flex min-h-screen w-full flex-col items-center gap-10 pt-30">
+      <main className="Serviços bg-primary flex min-h-screen flex-col items-center gap-10 pt-30">
         <h1 className="text-text circularBold text-center text-5xl">
           Fornecemos um serviço excelente <br /> para nossos clientes
         </h1>
-        <div className="cards grid grid-cols-1 place-items-center items-center justify-center gap-10 md:grid-cols-2 lg:grid-cols-4">
+
+        <div className="cards grid grid-cols-1 place-items-center items-center justify-center max-md:gap-20 gap-5 max-md:mb-10 md:grid-cols-2 2xl:grid-cols-4">
           {services.map((servico, index) => (
+
             <div
               key={index}
-              className="card hover:bg-terceary/50 bg-terceary relative flex w-60 flex-col rounded-2xl shadow-xl shadow-black transition-all duration-300 hover:shadow-2xl md:w-100"
+              className="card hover:bg-terceary/50 bg-terceary relative flex w-80 h-120 sm:w-90 lg:w-95 sm:h-150 lg:h-130 flex-col rounded-2xl shadow-xl shadow-black transition-all duration-300 hover:shadow-2xl"
             >
-              <div className="flex h-100 flex-col gap-4 px-5 py-7">
-                <div className="icon bg-primary/40 flex items-center justify-center rounded-lg outline outline-neutral-800 ease-in-out select-none md:h-18 md:w-18">
+              <div className="flex w-full items-center justify-center flex-col gap-4 px-5 py-7">
+                <div className="icon bg-primary/40 flex items-center justify-center rounded-lg outline outline-neutral-800 ease-in-out select-none h-18 w-18">
                   {servico.icon}
                 </div>
 
-                <div className="text text-text flex h-40 flex-col gap-1 pb-20">
+                <div className="text text-text flex items-center justify-center w-full h-40 flex-col gap-1 ">
                   <h2 className="title circularBold text-xl">
                     {servico.title}
                   </h2>
-                  <p className="description titilliumRegular text-base text-neutral-400">
+                  <p className="description titilliumRegular text-center text-base text-neutral-400">
                     {servico.description}
                   </p>
                 </div>
@@ -78,9 +80,10 @@ function Serviços() {
               <img
                 src={servico.image}
                 alt={servico.alt}
-                className="absolute -bottom-40 h-60 w-full rounded-lg object-cover select-none"
+                className="absolute sm:h-60 h-40 bottom-0 w-full rounded-lg object-cover select-none"
               />
             </div>
+
           ))}
         </div>{" "}
         {/* cards */}
